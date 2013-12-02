1e4fdaf5a7bd5aea21f892578af6568e4d0acc4d
Roman Nurik <roman@nurik.net>
2013-12-02 17:34:24 -0500

    Prefer Play Services fused location provider in Weather extension. Fall back to KitKat default SMS package in SMS extension (because of Hangouts).
    Change-Id: Id2cd6fc68a65484c87256cf64d73fedfa1759ea4

--- Android.mk
+++ Android.mk
@@ -6,8 +6,10 @@ LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-v4
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    android-support-v4 \
+    android-support-v13 \
+    play
 
 LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, res)
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
@@ -17,6 +19,16 @@ LOCAL_SRC_FILES += \
         src/com/google/android/apps/dashclock/api/internal/IExtension.aidl
 
 LOCAL_PACKAGE_NAME := DashClock
+
+LOCAL_JAVA_LIBRARIES := telephony-common
+
+# Include res dir from chips
+google_play_dir := ../../../external/google/google_play_services/libproject/google-play-services_lib/res
+res_dir := $(google_play_dir) res
+
+LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dir))
+LOCAL_AAPT_FLAGS := --auto-add-overlay
+LOCAL_AAPT_FLAGS += --extra-packages com.google.android.gms
 
 include $(BUILD_PACKAGE)
 
